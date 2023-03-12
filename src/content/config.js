4d5aab9b692acaa1ea09401ec01adca64e432742
Mohit Tyagi <mohit@hey.com>
2023-03-12 23:37:37 +0530

    feat: slug change

--- src/content/config.js
+++ src/content/config.js
@@ -1,7 +1,7 @@
 // 1. Import utilities from `astro:content`
 import { z, defineCollection } from 'astro:content';
 // 2. Define a schema for each collection you'd like to validate.
-const ideasSCollection = defineCollection({
+const fundamentalsCollection = defineCollection({
   schema: z.object({
     title: z.string(),
     description: z.string(),
@@ -12,5 +12,5 @@ const ideasSCollection = defineCollection({
 });
 // 3. Export a single `collections` object to register your collection(s)
 export const collections = {
-  'ideas': ideasSCollection,
+  'fundamentals': fundamentalsCollection,
 };
