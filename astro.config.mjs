0ec42ba04c5c63457f88535b38a3f5a2cdd100a3
Mohit Tyagi <mohit@hey.com>
2023-03-13 00:29:07 +0530

    feat: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
+  site: 'https://mohit.dev',
+  integrations: [tailwind(), mdx(), sitemap()]
 });
